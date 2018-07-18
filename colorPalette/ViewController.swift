0df4b355360e3a578e25d0d2b298989878b5eb62
Dynnikov Oleg <dinnikovoleg@gmail.com>
2018-07-18 13:04:40 +0300

    Added palette functional to VC

--- colorPalette/ViewController.swift
+++ colorPalette/ViewController.swift
@@ -10,16 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redColorValueLabel: UILabel!
+    @IBOutlet weak var greenColorValueLabel: UILabel!
+    @IBOutlet weak var blueColorValueLabel: UILabel!
+    @IBOutlet weak var paletteView: UIView!
+    @IBOutlet weak var redSliderValueLabel: UISlider!
+    @IBOutlet weak var greenSliderValueLabel: UISlider!
+    @IBOutlet weak var blueSliderValueLabel: UISlider!
+
+    let defaultMinValue: Float = 0.0
+    let defaultMaxValue: Float = 255.0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        redColorValueLabel.text = "\(Int(redSliderValueLabel.value))"
+        greenColorValueLabel.text = "\(Int(greenSliderValueLabel.value))"
+        blueColorValueLabel.text = "\(Int(blueSliderValueLabel.value))"
+        changedColor()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func minPaletteValueButton(_ sender: Any) {
+        addedValueLabel(value: defaultMinValue)
+        changedColor()
     }
 
+    @IBAction func mediumPaletteValueButton(_ sender: Any) {
+        addedValueLabel(value: (defaultMaxValue - defaultMinValue) / 2 + defaultMinValue)
+        changedColor()
+    }
 
+    @IBAction func maxPaletteValueButton(_ sender: Any) {
+        addedValueLabel(value: defaultMaxValue)
+        changedColor()
+    }
+
+    @IBAction func redChangedSlider(_ sender: Any) {
+        changedColor()
+    }
+
+    @IBAction func greenChangedSlider(_ sender: Any) {
+        changedColor()
+    }
+
+    @IBAction func blueChangedSlider(_ sender: Any) {
+        changedColor()
+    }
+
+    func changedColor() {
+        paletteView.backgroundColor = UIColor(red: CGFloat(redSliderValueLabel.value / 255.0), green: CGFloat(greenSliderValueLabel.value / 255.0), blue: CGFloat(blueSliderValueLabel.value / 255.0), alpha: 1.0)
+        redColorValueLabel.text = "\(Int(redSliderValueLabel.value))"
+        greenColorValueLabel.text = "\(Int(greenSliderValueLabel.value))"
+        blueColorValueLabel.text = "\(Int(blueSliderValueLabel.value))"
+    }
+
+    func addedValueLabel(value: Float) {
+        redSliderValueLabel.value = value
+        greenSliderValueLabel.value = value
+        blueSliderValueLabel.value = value
+    }
 }
 
